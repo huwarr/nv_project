c27b300f3cb1326843218121f5468e557a013e31
huwarr <mikhalmatova@gmail.com>
2022-12-21 23:57:25 +0300

    add link to checkpoint

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ pip install -r requirements.txt
 pip install gdown==4.5.4 --no-cache-dir
 
 # Download HiFiGAN checkpoint
-gdown ...
+gdown https://drive.google.com/uc?id=1BhaVPY2wwpGWAT_rIPmGxjiBbR2eKmcu
