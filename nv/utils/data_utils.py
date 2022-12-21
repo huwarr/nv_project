remove schedulers + fix melspec computation

nv/utils/data_utils.py
@@ -2,6 +2,7 @@ import os
 
 import torch
 from torch import nn
+import torch.nn.functional as F
 from tqdm.auto import tqdm
 import torchaudio
 import librosa
@@ -22,7 +23,9 @@ class MelSpectrogram(nn.Module):
             n_fft=config.n_fft,
             f_min=config.f_min,
             f_max=config.f_max,
-            n_mels=config.n_mels
+            n_mels=config.n_mels,
+            center=False,
+            pad=config.pad_value
         )
 
         # The is no way to set power in constructor in 0.5.0 version.
@@ -44,6 +47,9 @@ class MelSpectrogram(nn.Module):
         :param audio: Expected shape is [B, T]
         :return: Shape is [B, n_mels, T']
         """
+
+        audio = F.pad(audio.unsqueeze(1), ((self.config.n_fft - self.config.hop_length) // 2, (self.config.n_fft - self.config.hop_length) // 2), mode='reflect')
+        audio = audio.squeeze(1)
 
         mel = self.mel_spectrogram(audio) \
             .clamp_(min=1e-5) \

train.py
@@ -157,8 +157,8 @@ for epoch in range(train_config.epochs):
             # nn.utils.clip_grad_norm_(
             #     generator.parameters(), train_config.grad_clip_thresh
             # )
-            scheduler_d.step()
-            scheduler_g.step()
+            # scheduler_d.step()
+            # scheduler_g.step()
 
             if current_step % train_config.save_step == 0:
                 torch.save(